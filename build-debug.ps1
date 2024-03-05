16e16be20be1a3f9af96f174ba9c53c8f41825e1
Ian Elario <16409028+IanE-Official@users.noreply.github.com>
2024-03-05 04:59:19 -0800

    Delete build-debug.ps1

--- build-debug.ps1
+++
@@ -1,3 +0,0 @@
-Remove-Item $PSScriptRoot/build -Recurse -Force -ErrorAction:SilentlyContinue -Confirm:$False | Out-Null
-& cmake -B $PSScriptRoot/build -S $PSScriptRoot/Plugin --preset=build-debug-msvc-msvc -DCMAKE_TOOLCHAIN_FILE="$PSScriptRoot/extern/vcpkg/scripts/buildsystems/vcpkg.cmake"
-& cmake --build $PSScriptRoot/build --config Debug
